6fa3e1ec6b7a0eeb966f7d7a695d6934c30e68e4
JakeLin <jakelin0fly@gmail.com>
2019-07-31 10:04:40 +0800

    新建Demo

---
+++ QTitleBarDemo.cpp
@@ -0,0 +1,11 @@
+#include "QTitleBarDemo.h"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+{
+}
+
+Widget::~Widget()
+{
+
+}
---
+++ QTitleBarDemo.h
@@ -0,0 +1,15 @@
+#ifndef QTITLEBARDEMO_H
+#define QTITLEBARDEMO_H
+
+#include <QWidget>
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = 0);
+    ~Widget();
+};
+
+#endif // QTITLEBARDEMO_H
