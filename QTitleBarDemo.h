e4adb7463da88aad4f2153ed78d4cf2f12e42be1
JakeLin <jakelin0fly@gmail.com>
2019-07-31 14:39:53 +0800

    新增标题栏元素变量 点击最小化、最大化、还原、关闭信号!

--- QTitleBarDemo.h
+++ QTitleBarDemo.h
@@ -2,6 +2,10 @@
 #define QTITLEBARDEMO_H
 
 #include <QWidget>
+#include <QLabel>
+#include <QPushButton>
+#include <QMenu>
+
 
 class Widget : public QWidget
 {
@@ -10,6 +14,34 @@ class Widget : public QWidget
 public:
     Widget(QWidget *parent = 0);
     ~Widget();
+
+public:
+signals:
+    void signalMinButtonClicked();    //点击最小化信号
+    void signalRestoreButtonClicked();    //点击还原信号
+    void signalMaxButtonClicked();    //点击最大化信号
+    void signalCloseButtonClicked();    //点击关闭信号
+
+
+private:
+    QLabel *titleIconLabel;  //图标
+    QLabel *titleText;  //标题栏 文本
+
+    QMenu *menu;    //标题栏菜单
+    QAction *userAction;    //用户信息
+    QAction *settingAction; //设置
+    QAction *aboutAction;   //关于
+
+    QPushButton *minPushButton; //最小化
+    QPushButton *restorePushButton; //还原
+    QPushButton *maxPushButton;     //最大化
+    QPushButton *closePushButton;   //关闭
+
+    QPoint restorePoint;    //最大化前窗口位置
+    QSize restoreSize;  //最大化前窗口大小
+
+    QPoint mouseMoveStartPoint;  //鼠标点击移动起始位置
+
 };
 
 #endif // QTITLEBARDEMO_H
